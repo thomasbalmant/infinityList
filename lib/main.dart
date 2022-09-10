f6cc004df7b50a91c48c1fe2f41ca7862c224fc1
thomasbalmant <thomas_dru@hotmail.com>
2022-09-10 16:26:38 -0300

    list done

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,49 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return MaterialApp(
-      title: 'Infinity List',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Names List')),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        ),
+    // ignore: prefer_const_constructors
+    return MaterialApp(title: 'Name Generator', home: const RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Name Generator'),
       ),
+      body: _buildSuggestions(context),
+    );
+  }
+
+  Widget _buildSuggestions(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
     );
   }
 }
