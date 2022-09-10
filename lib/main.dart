126d7ce093be680748af36daa56ccd4c20e5c22e
thomasbalmant <thomas_dru@hotmail.com>
2022-09-10 15:29:04 -0300

    change screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main(List<String> args) {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Infinity List',
       home: Scaffold(
         appBar: AppBar(title: const Text('Names List')),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
